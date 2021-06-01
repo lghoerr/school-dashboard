e326386b3e440e7a37727e511fb79a8e3368b063
Andrew Niu <andrewmniu@gmail.com>
2021-06-01 10:09:41 -0400

    Created GET endpoints

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
 const port = 5000;
-const app = express()
+const app = express();
 
 const db = require("./db.js");
 
@@ -9,10 +9,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Test')
-})
+app.get("/teachers", (req, res) => {
+  const teachers = [];
+  db.collection("Teachers")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((teacher) => {
+        teachers.push({...teacher.data(), id: teacher.id});
+      });
+    })
+    .then(() => res.json(teachers));
+});
+
+app.get("/students", (req, res) => {
+  const students = [];
+  db.collection("Students")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((student) => {
+        students.push(student.data());
+      });
+    })
+    .then(() => res.json(students));
+});
+
+app.get("/classes", (req, res) => {
+  const classes = [];
+  db.collection("Classes")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((cls) => {
+        classes.push(cls.data());
+      });
+    })
+    .then(() => res.json(classes));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
