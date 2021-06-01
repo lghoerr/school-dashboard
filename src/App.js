392fe98320e1b9d39f2ee8bdd1ba8dc46eeb77ae
lghoerr <63504311+lghoerr@users.noreply.github.com>
2021-06-01 10:16:22 -0400

    navbar link to home

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+      </Switch>
       <HomePage />
     </Router>
   );
