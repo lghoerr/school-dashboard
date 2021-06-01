ade4a0162ba9176e6d838476227f849801497f8b
David Hasani <davidh.01@icloud.com>
2021-06-01 12:57:41 -0400

    added title to login page

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -5,6 +5,7 @@ const SignIn = () => {
 
     return (
         <div style={{marginTop:20}}>
+            <h2>Admin Access</h2>
             <Input type="text" placeholder="username"></Input> <br /> <br />
             <Input type="text" placeholder="password"></Input> <br /> <br />
             <Button variant="contained" color="primary">Sign In</Button>
